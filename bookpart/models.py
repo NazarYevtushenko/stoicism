5fcc37ead50b1a246af90779354b80847c3f2f68
Yevtushenko Nazar <52324761+NazarYevtushenko@users.noreply.github.com>
2023-01-02 21:18:05 +0200

    add models and urls

--- bookpart/models.py
+++ bookpart/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(blank=True, max_length=255)
+    main_text = models.CharField(blank=True, max_length=1024)
+    data = models.DateField(blank=True)
